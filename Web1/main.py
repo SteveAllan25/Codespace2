7e0511a70178aba153af93218cd7adef5c279f6a
SteveAllan25 <jasonalando2@gmail.com>
2025-05-22 12:28:59 +0000

    changes made

---
+++ Web1/main.py
@@ -0,0 +1,19 @@
+# This is a comment
+print("its  going well")
+num = 1
+num2 = 2
+
+if num > num2:
+    print("num is greater than num2")
+elif num < num2:
+    print("num2 is grejdhhhter than num")
+else:
+    print("num is equal to num2")
+    
+    
+
+
+
+
+
+
